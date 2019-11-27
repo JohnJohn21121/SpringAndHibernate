5cd5d9ea961bf15c4fc7802f92b236b91bf3853c
JohnJohn21121 <john.abreu21121@gmail.com>
2019-11-27 09:52:14 -0300

    added Arrays to RandomFortuneService

--- DevSpring/SpringDemoAnotations/src/com/JohnJohn21121/RandomFortuneService.java
+++ DevSpring/SpringDemoAnotations/src/com/JohnJohn21121/RandomFortuneService.java
@@ -2,12 +2,27 @@ package com.JohnJohn21121;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    private String[] data = {
+      "You cannot foretell the consequences of your actions.",
+            "Diligence is the Mother of good luck.",
+            "The thousand miles Road is Fulfilled step by step."
+    };
+
+    private Random randomNumber = new Random();
+
     @Override
     public String getDailyFortune() {
-        return "You Cannot foretell the consequences of your actions.";
+
+        int index = randomNumber.nextInt(data.length);
+
+        String theRandomFortune = data[index];
+
+        return theRandomFortune;
     }
 
 }
